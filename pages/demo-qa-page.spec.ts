ca157acc602be7c9e575e055dcbe50ce245df083
Nuntapong <nuntapong@odds.team>
2023-02-02 13:21:59 +0700

    [e2e]: user click text elements

---
+++ pages/demo-qa-page.spec.ts
@@ -0,0 +1,17 @@
+import { expect, Page } from "@playwright/test";
+
+export class DemoQaPage {
+  readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async expectUrl() {
+    await expect(this.page).toHaveURL("https://demoqa.com/");
+  }
+
+  async clickElement(element: string) {
+    await this.page.click(element);
+  }
+}
--- tests/elements-test.spec.ts
+++ tests/elements-test.spec.ts
@@ -1,91 +1,15 @@
-import { expect, test } from "@playwright/test";
+import { test } from "@playwright/test";
+import { DemoQaPage } from "../pages/demo-qa-page.spec";
 
 // go to link https://demoqa.com/
 test.beforeEach(async ({ page }) => {
   await page.goto("https://demoqa.com/");
 });
 
-test("ผู้ใช้งานกรอกข้อมูลใน tab Text Box แล้่วกดปุ่ม Submit", async ({
-  page,
-}) => {
-  await page.click("text=Elements");
-  await page.click("text=Text Box");
-  await page
-    .getByPlaceholder("Full Name")
-    .type("Nuntapong Siripanyawong", { delay: 100 });
-  await page
-    .getByPlaceholder("name@example.com")
-    .type("nuntapong@gmail.com", { delay: 100 });
-  await page
-    .getByPlaceholder("Current Address")
-    .type("Bangkok", { delay: 100 });
-
-  const permanentAddress = await page.$("#permanentAddress.form-control");
-  await permanentAddress?.type("Phuket", { delay: 100 });
-  await page.click("text=Submit");
-
-  await expect(page.getByText(/Nuntapong Siripanyawong/i)).toBeVisible();
-  await expect(page.getByText(/nuntapong@gmail.com/i)).toBeVisible();
-  await expect(page.getByText(/Bangkok/i)).toBeVisible();
-  await expect(page.getByText(/Phuket/i)).toBeVisible();
-});
-
-test("ผู้ใช้งานกดปุ่ม checkbox ใน tab checkbox", async ({ page }) => {
-  await page.click("text=Elements");
-  await page.click("text=Check Box");
-  await page.check("text=Home");
-});
-
-test("ผู้ใช้งานกดปุ่ม radio Yes ใน tab Radio button", async ({ page }) => {
-  await page.click("text=Elements");
-  await page.click("text=Radio Button");
-  await page.check("text=Yes");
-});
-
-test("ผู้ใช้งานกดปุ่ม radio Impressive ใน tab Radio button", async ({
-  page,
-}) => {
-  await page.click("text=Elements");
-  await page.click("text=Radio Button");
-  await page.check("text=Impressive");
-});
-
-test("ผู้ใช้งานกรอกข้อมูลใน tab Web Tables", async ({ page }) => {
-  await page.click("text=Elements");
-  await page.click("text=Web Tables");
-  await page.click("id=addNewRecordButton");
-  await page.getByPlaceholder("First Name").type("Nuntapong", { delay: 100 });
-  await page
-    .getByPlaceholder("Last Name")
-    .type("Siripanyawong", { delay: 100 });
-  await page
-    .getByPlaceholder("name@example.com")
-    .type("nuntapong@gmail.com", { delay: 100 });
-  await page.getByPlaceholder("Age").type("25", { delay: 100 });
-  await page.getByPlaceholder("Salary").type("10000", { delay: 100 });
-  await page.getByPlaceholder("Department").type("Developer", { delay: 100 });
-  await page.click("text=Submit");
-  await page.getByPlaceholder("Search").type("Nuntapong", { delay: 100 });
-});
-
-test("ผู้ใช้งานกดปุ่ม Double click me button ใน tab Buttons", async ({
-  page,
-}) => {
-  await page.click("text=Elements");
-  await page.click("text=Buttons");
-  await page.dblclick("text=Double click me");
-});
-
-test("ผู้ใช้งานกดปุ่ม Right click me button ใน tab Buttons", async ({
-  page,
-}) => {
-  await page.click("text=Elements");
-  await page.click("text=Buttons");
-  await page.click("text=Right click me", { button: "right" });
-});
-
-test("ผู้ใช้งานกดปุ่ม Click me button ใน tab Buttons", async ({ page }) => {
-  await page.click("text=Elements");
-  await page.click("text=Buttons");
-  await page.click("text=Click me");
+test.describe("ผู้ใช้งานกรอกข้อมูลใน tab Text Box", () => {
+  test("ผู้ใช้กรอกข้อมูลใน tab Text box ครบถ้วน", async ({ page }) => {
+    const demoQaPage = new DemoQaPage(page);
+    await demoQaPage.expectUrl();
+    await demoQaPage.clickElement("text=Elements");
+  });
 });
