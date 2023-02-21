07c9dd3fb3e64d7fff195f83a32a66bf12bbc55b
Nuntapong <nuntapong@odds.team>
2023-02-21 08:23:34 +0700

    [e2e]: user input data in tab practice form

--- tests/form-test.spec.ts
+++ tests/form-test.spec.ts
@@ -1,6 +1,13 @@
 import { test } from "@playwright/test";
+import { DemoQaPage } from "../pages/elements-qa-page.spec";
 
 // go to link https://demoqa.com/
 test.beforeEach(async ({ page }) => {
   await page.goto("https://demoqa.com/");
+});
+
+describe("ผู้ใช้งานกรอกข้อมูลใน Tab Forms ครบถ้วน", () => {
+  test("ผู้ใช้งานกรอกข้อมูลใน tab Practice Form", async ({ page }) => {
+    const demoqa = new DemoQaPage(page);
+  });
 });
