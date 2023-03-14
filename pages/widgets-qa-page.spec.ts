5be9e4f20421d3f6e48b5848f3ff2cb606f6c738
NuntapongS <nuntapong@odds.team>
2023-03-14 08:02:58 +0700

    [e2e]: expect header origin in tab tabs

--- pages/widgets-qa-page.spec.ts
+++ pages/widgets-qa-page.spec.ts
@@ -195,8 +195,12 @@ export class DemoQaPageWidgets {
   async expectTextInFieldWhat() {
     await expect(
       this.page.getByText(
-        /Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum./i
+        /Lorem Ipsum is simply dummy text of the printing and typesetting industry./i
       )
     ).toBeVisible();
+  }
+
+  async expectHeaderOrigin() {
+    await expect(this.page.getByRole("tab", { name: "Origin" })).toBeVisible();
   }
 }
