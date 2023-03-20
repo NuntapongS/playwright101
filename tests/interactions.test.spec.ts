e9296e11278357fbc67bd5d68308221ca1bfc517
NuntapongS <nuntapong@odds.team>
2023-03-20 08:49:35 +0700

    [e2e]: prepare to test step interaction

---
+++ tests/interactions.test.spec.ts
@@ -0,0 +1,13 @@
+import { test } from "@playwright/test";
+import { DemoQaPageInteractions } from "../pages/interaction.spec";
+
+// go to link https://demoqa.com/
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://demoqa.com/");
+});
+
+test.describe("ผู้ใช้งานลากข้อมูลใน step Interactions", () => {
+  test("ผู้ใช้งาน sorting ใน tab sortable", async ({ page }) => {
+    const DemoQaPage = new DemoQaPageInteractions(page);
+  });
+});
