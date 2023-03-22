53d5ee3110d824257bae9506e852c97f77763ed5
NuntapongS <nuntapong@odds.team>
2023-03-22 08:47:58 +0700

    [e2e]: verify when user click header grid

--- tests/interactions.test.spec.ts
+++ tests/interactions.test.spec.ts
@@ -38,6 +38,7 @@ test.describe("ผู้ใช้งานลากข้อมูลใน step
     await demoQaPage.clickItem3("text=Morbi leo risus");
     await demoQaPage.expectTextInItem4();
     await demoQaPage.clickItem4("text=Porta ac Consectetur ac");
-    await demoQaPage.expectTextGrid("text=Grid");
+    await demoQaPage.expectTextGrid();
+    await demoQaPage.clickGrid("text=Grid");
   });
 });
